fd1087f412be7362ed68df563348cf6d8270caec
huy.pham <huy.pham@rikai.technology>
2022-01-11 11:18:47 +0700

    Implement change file's name, resize image and generate thumbnail

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,26 +1,66 @@
 import { Inject, Injectable, HttpException, HttpStatus, UploadedFile, UploadedFiles } from '@nestjs/common';
+import { extname } from 'path';
+import { GlobalService } from 'src/utils/global.service';
 
 
 @Injectable()
 export class UploadService {
 
-    async uploadedFile(@UploadedFile() file) {
+    async uploadedFile(userId: string, @UploadedFile() file) {
+      const fs = require('fs');
+      const sharp = require('sharp');
+      const now = Date.now();
+      const fileExtName = extname(file.originalname);
+      const uploadFileName = GlobalService.rootPath + GlobalService.uploadPrefix + userId + '-' + now + fileExtName;
+      const thumbnailFileName = GlobalService.rootPath + GlobalService.thumbnailsPrefix + userId + '-' + now + fileExtName;
+      fs.rename(file.path , uploadFileName, function (err) {
+        if (err) throw err;
+        console.log('File Renamed.');
+      });
+      sharp(uploadFileName).resize({
+        width: GlobalService.thumbnailsImageWidth,
+        height: GlobalService.thumbnailsImageHeight
+      }).toFile(thumbnailFileName, function(err) {
+        if (err) {
+            console.error('sharp>>>', err)
+        }
+        console.log('ok okoko')
+    })
       const response = {
-        originalname: file.originalname,
-        filename: file.filename,
+        upload: uploadFileName,
+        thumbnail: thumbnailFileName,
       };
       return response;
     }
 
-    async uploadMultipleFiles(@UploadedFiles() files) {
-        const response = [];
-        files.forEach(file => {
-          const fileReponse = {
-            originalname: file.originalname,
-            filename: file.filename,
-          };
-          response.push(fileReponse);
+    async uploadMultipleFiles(userId: string, @UploadedFiles() files) {
+      const response = [];
+      files.forEach(file => {
+        const fs = require('fs');
+        const sharp = require('sharp');
+        const now = Date.now();
+        const fileExtName = extname(file.originalname);
+        const newFileName = GlobalService.rootPath + GlobalService.uploadPrefix + userId + '-' + now + fileExtName;
+        const thumbnailFileName = GlobalService.rootPath + GlobalService.thumbnailsPrefix + userId + '-' + now + fileExtName;
+        fs.rename(file.path , newFileName, function (err) {
+          if (err) throw err;
+          console.log('File Renamed.');
         });
-        return response;
-      }
+        sharp(newFileName).resize({
+          width: GlobalService.thumbnailsImageWidth,
+          height: GlobalService.thumbnailsImageHeight
+        }).toFile(thumbnailFileName, function(err) {
+          if (err) {
+              console.error('sharp>>>', err)
+          }
+          console.log('ok okoko')
+      })
+        const fileReponse = {
+          upload: newFileName,
+          thumbnail: thumbnailFileName,
+        };
+        response.push(fileReponse);
+      });
+      return response;
+    }
 }
---
+++ src/utils/global.service.ts
@@ -0,0 +1,8 @@
+export class GlobalService{ 
+    static rootPath = './files/'; 
+    static uploadPrefix = 'upload-';
+    static thumbnailsPrefix = 'thumbnails-';
+    static thumbnailsImageWidth = 42;
+    static thumbnailsImageHeight = 42;
+ }
+ 
