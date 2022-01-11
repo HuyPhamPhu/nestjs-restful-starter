8665bd16ac32f32cb0f153896bc40a350a5a2bb2
huy.pham <huy.pham@rikai.technology>
2022-01-11 11:18:47 +0700

    Implement change file's name, resize image and generate thumbnail
    Last commands done (2 commands done):

--- src/upload/file-upload.utils.ts
+++ src/upload/file-upload.utils.ts
@@ -1,16 +1,7 @@
-import { extname } from "path";
-
 export const imageFileFilter = (req, file, callback) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
       return callback(new Error('Only image files are allowed!'), false);
     }
     callback(null, true);
-  };
-
-  export const editFileName = (request, file, callback) => {
-    const name = file.originalname.split('.')[0];
-    const fileExtName = extname(file.originalname);
-    const randomName = Date.now();
-    callback(null, `${name}-${randomName}${fileExtName}`);
   };
   
