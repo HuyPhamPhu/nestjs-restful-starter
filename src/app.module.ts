dbf9530090db9334f77d3feaf4090fee9dd1c3bc
rikaibinhhoang <binh.hoang@rikai.technology>
2022-01-13 15:45:33 +0700

    update search feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,16 @@ import { PostsModule } from './posts/posts.module';
 import { UploadModule } from './upload/upload.module';
 import { SearchModule } from './search/search.module';
 import { SocketInsideModule } from './socket-inside/socket-inside.module';
-
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        ELASTICSEARCH_NODE: Joi.string().required(),
+      }),
+    }),
     TasksModule,
     UsersModule,
     PostsModule,
--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     ElasticsearchModule.registerAsync({
-      useFactory: async () => ({
-        node: 'http://localhost:9200',
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get('ELASTICSEARCH_NODE'),
       }),
+      inject: [ConfigService],
     }),
   ],
   exports: [ElasticsearchModule],
